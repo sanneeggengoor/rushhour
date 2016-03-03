32fc8bb1ce7894a14a7612833c763e49c530e4fc
CharlotteSmoor <charlottesmoor@gmail.com>
2016-03-03 12:10:47 +0100

    hoi

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -13,13 +13,12 @@ public class Grid {
   public Grid(int rows, int columns){
       this.rows = rows;
       this.columns = columns;
-      grid = new int[rows - 1][columns - 1];
-      for(int i = 0; i < rows; i++){
+      grid = new int[rows][columns];
+      for(int i = 0; i<rows; i++){
           for(int j = 0; j<columns; j++){
               grid[i][j] = 0;
           }
       }
-
     }
 
     public int[][] getGrid(){
